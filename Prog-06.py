99f9d71055f7e36cd0cccf238081f4b4b90347aa
choudharyusman <45047731+choudharyusman@users.noreply.github.com>
2018-11-25 19:51:06 +0500

    Update Prog-06.py

--- Prog-06.py
+++ Prog-06.py
@@ -1,19 +1,28 @@
-print("NEWTON'S LAWS OF MOTION")
-print(u"\u25A0"*23)
+print("LAWS OF MOTION")
+print(u"\u25A0"*14)
+import math
+def law_1(u,t):
+    v= u+(9.81*t)
+    print("The final velocity at an initial velocity of: "+str(u)+"m/s and Time "+str(t)+"s is: "+str(v)+"m/s")
+def law_2(u,t):
+    s=u*t+(1/2*9.81*t**2)
+    print("The distance covered at an initial velocity of: "+str(u)+"m/s and Time "+str(t)+"s is: "+str(s)+"m")
+def law_3(u,s):
+    v= math.sqrt(u**2+2*(9.81*s))
+    print("The final velocity at an initial velocity of: "+str(u)+"m/s and distance: "+str(s)+"m is: "+str(v))
 
-def law_1():
-    print("Newton's 1st law of motion\n",u"\u25A0"*25)
-    print("It states only that a body at rest will remain at rest unless an outside force acts on it, and a body in motion at a constant velocity will remain in motion in a straight line unless acted upon by an outside force. ")
-def law_2(m,a):
-    print("Newton's 2nd law of motion\n", u"\u25A0"*25)
-    print("The second law states that the acceleration of an object is dependent upon two variables - the net force acting upon the object and the mass of the object.")
-    F= m * a
-    return F
-def law_3():
-    print("Newton's 3rd law of motion\n", u"\u25A0"*25)
-    print("The Third Law of Motion states:\nFor every action, there is an equal and opposite reaction.")
-print(law_1())
-m=eval(input("Enter mass of object: "))
-a=eval(input("Enter acceleration of object: "))
-print(law_2(m,a))
-print(law_3())
+user_inp= input("Please enter the desired law you wish to calculate: 1,2 or 3: ")
+if user_inp=='1':
+    u=eval(input("Please enter the initial velocity: "))
+    t= eval(input("Please enter the time taken:      "))
+    law_1(u,t)
+if user_inp=='2':
+    u=eval(input("Please enter the initial velocity: "))
+    t= eval(input("Please enter the time taken:      "))
+    law_2(u,t)
+if user_inp=='3':
+    u=eval(input("Please enter the initial velocity: "))
+    s=eval(input("Please enter the distance covered: "))
+    law_3(u,s)
+else:
+    print("Please Enter 1,2 or 3")
